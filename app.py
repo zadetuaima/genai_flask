5de0002965c3b2edd329845995a1c428dd0bc592
zadetuaima <zade.tuaima@hogarth.com>
2024-11-27 16:23:04 +0000

    working index.html + app.py + bot.py

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template, request
-from bot.py import TODO#function to define
+from bot import ask_question
 
+# app is a variable representing
+# our flask app
+# __name__ is a python reserved
+# word
+# telling Flask where our code
+# lives
 app = Flask(__name__)
 
 # set up our landing page
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('index.html', question="Type question here!", chatbot_response = "")
 
 # only use this when posting data!
 @app.route('/', methods=['POST'])
 def index_post():
 	user_question = request.form['req_question']
-	return render_template('index.html')
+	chatbot_response = ask_question(user_question)
+	return render_template('index.html', question = user_question, chatbot_response = chatbot_response)
--- bot.py
+++ bot.py
@@ -4,109 +4,103 @@ import requests
 import json
 
 client = AzureOpenAI(
-    api_key = os.getenv("AZURE_KEY"),
-    azure_endpoint = os.getenv("AZURE_ENDPOINT"),
-    api_version = "2023-10-01-preview"
+	api_key = os.getenv("AZURE_KEY"),
+	azure_endpoint = os.getenv("AZURE_ENDPOINT"),
+	api_version = "2023-10-01-preview"
 )
 
-
-#inital chat input message
 messages = [
-    {"role":"user",
-     "content":"Find the current price of etherium in GBP and return the answer as a poem"}
+	{"role": "system", "content": "Respond to everything as a short poem"},
 ]
 
-
-#helper function for crypto prices
-#so fetches current price of crypto in a specifci currency via coingecko API
-
-#this is API specific to whatever one you are using
 def crypto_price(crypto_name, fiat_currency):
-    url = f"https://api.coingecko.com/api/v3/coins/markets?vs_currency={fiat_currency}"
-    response = requests.get(url)
-    data = response.json()
-    current_price = [coin["current_price"] for coin in data if coin["id"] == crypto_name][0]
-    return f"The current price of {crypto_name} is {current_price} {fiat_currency} at the moment!"
+	url = f"https://api.coingecko.com/api/v3/coins/markets?vs_currency={fiat_currency}"
+	print(url)
+	response = requests.get(url)
+	data = response.json()
+	# my_coin is the selected dictionary with all crypto data for selected crypto
+	current_price = [coin['current_price'] for coin in data if coin['id'] == crypto_name][0]
+	return f"The current price of {crypto_name} is {current_price} {fiat_currency}"
 
-#GPT metadata function
-#This provides the GPT with metadata about a custom function, so this but is essential for initalising the GPT
 functions = [
 	{
 		"type": "function",
 		"function": {
 			"name": "get_crypto_price",
-			"description": "Gets prices of a cryptocurrency in a specifc currency",
+			"description": "Gets prices of crypto currency in a specified global currency",
 			"parameters": {
+				# lettings chat-gpt know that it's getting
+				# key-value pairs
 				"type": "object",
 				"properties": {
 					"crypto_name": {
-                    "type": "string",
-                    "description": "the name of the crypto currency that I want to look at"
-                    },
-                    "fiat_currency": {
-                        "type": "string",
-                        "description": "The fiat currency for definding the price of crypto currency using the official currency abbreviation"
-                    }
-                },
-                "required": ["cyrpto_name", "fiat_currency"]
+						"type": "string",
+						"description": "The name of the crypto currency that I want to look up"
+					},
+					"fiat_currency": {
+						"type": "string",
+						"description": "The fiat currency for defining the price of crypto currency. Use the official abbreviation"
+					}
+				},
+				"required": ["crypto_name", "fiat_currency"]
 			}
 		}
 	}
 ]
 
 
-#This is the initial chat completion request, where it send the user message and function description to the gpt
-#bascially routes all the info above to the openAI API so it can get an answer
-response = client.chat.completions.create(
-    model = "GPT-4",
-    messages = messages,
-    tools = functions,
-    #auto means chatgpt decides when it wants to call this function
-    tool_choice = "auto"
-)
 
-#response_message is the initial response of the GPT
-response_message = response.choices[0].message
-#gpt_tools determines if the GPT needs external data
-gpt_tools = response.choices[0].message.tool_calls
+def ask_question(user_question):
+	# we're building a converation, and saying every message
+	# from the user
+	messages.append({"role": "user", "content": user_question})
 
-#gpt_tools is a list
-if gpt_tools: #if GPT tools is being requested...
-    #so we want to set up a 'phonebook', where if we see a function name we are able to tell our code which fucntion to tool_calls
-    #bit below is the 'phonebook'
-    avaliable_functions = {
-        "get_crypto_price": crypto_price
-    }
+	response = client.chat.completions.create(
+		model = "GPT-4",
+		messages = messages,
+		tools = functions,
+		# auto means chat-gpt decides when it needs to use external functions
+		tool_choice = "auto"
+	)
 
-    #this whole bit below is updated the convo history, so that the bot doesn't get confused when you keep sending it stuff
-    messages.append(response_message)
-    for gpt_tool in gpt_tools: #loops through each tool call requested by the GPT
-        #so at this point, gpt is going to want to decide which bit of infomation they want to grab,
-        #so in this case it looks up the fucntuon
-        function_name = gpt_tool.function.name
-        #here I'm looking up the functuon name within the 'phonebook'
-        function_to_call = avaliable_functions[function_name] #Looks up the corresponding Python function in the avaliable_functions dictionary
-        # need the parameters for the function
-        function_parameters = json.loads(gpt_tool.function.arguments) #this contains the parameters of the function
-        function_response = function_to_call(function_parameters.get("crypto_name"), function_parameters.get("fiat_currency")) #this bit then executes the function
+	response_message = response.choices[0].message
+	# if chat-gpt doesn't need help, this will be None, otherwise there will be stuff
+	gpt_tools = response.choices[0].message.tool_calls
 
-        #this appends the tool response to the convo
-        messages.append(
-			{
-				"tool_call_id": gpt_tool.id,
-				"role": "tool",
-				"name": function_name,
-				"content": function_response
-			}
-		)
+	# if gpt_tools is not None
+	# gpt_tools is a list!
+	if gpt_tools:
 
-        #then this requests a follow-up response from the GPT
-        second_response = client.chat.completions.create(
-			model = "GPT-4",
-			messages = messages
-		)
-        print(second_response.choices[0].message.content)
+		# set up a 'phonebook', if we see a function name, we need to tell our code
+		# which function to call
+		available_functions = {
+			"get_crypto_price": crypto_price
+		}
 
-#and if the GPT doesn't need any tools, i.e. if the initial question doesnt relate to crypto in this case it will just print a repsonse as normal
-else:
-    print(response.choices[0].message.content)
+		messages.append(response_message)
+		for gpt_tool in gpt_tools:
+			# figure out which friend to call
+			function_name = gpt_tool.function.name
+			# looking up my function name in my 'phonebook'
+			function_to_call = available_functions[function_name]
+			# need the parameters for the function
+			function_parameters = json.loads(gpt_tool.function.arguments)
+			function_response = function_to_call(function_parameters.get('crypto_name'), function_parameters.get('fiat_currency'))
+			messages.append(
+				{
+					"tool_call_id": gpt_tool.id,
+					"role": "tool",
+					"name": function_name,
+					"content": function_response
+				}
+			)
+			second_response = client.chat.completions.create(
+				model = "GPT-4",
+				messages=messages
+			)
+			# this response happens if you use the crypto function
+			return second_response.choices[0].message.content
+
+	else:
+		# this is the chatbot response if no external function is used
+		return response.choices[0].message.content
